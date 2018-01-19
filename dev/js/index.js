2b20555da4495f8f1c811c3f4521c21a8bc3ddb1
bgirlcoolcat <evansb21@gmail.com>
2018-01-19 13:03:48 +0000

    Set up our store

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,9 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
 
+const store = createStore();
 
 ReactDOM.render(
     <h1>Hello World!</h1>, 
     document.getElementById('root')
 );
+
+// Step 1 - we set up our store using the syntax above
