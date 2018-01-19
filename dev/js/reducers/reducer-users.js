262c0b406040d07fd53e2493df2e764a0eb4556f
bgirlcoolcat <evansb21@gmail.com>
2018-01-19 12:27:59 +0000

    initial commit

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,42 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: 'Bucky',
+      last: 'Roberts',
+      age: 71,
+      description: 'I like bacon!',
+      thumbnail: 'http://i.imgur.com/7yUvePI.jpg'
+    },
+    {
+      id: 2,
+      first: 'Noddy',
+      last: 'Boy',
+      age: 50,
+      description: 'I live in Toytown',
+      thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8x8T2_oRGzxF3jRAzXTjp44ejUF6AqY3UoX-F6MnKd4Tav_rT_A'
+    },
+    {
+      id: 3,
+      first: 'Big',
+      last: 'Ears',
+      age: 60,
+      description: 'I hang out with Noddy',
+      thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQammUAUwDweDex-RaD7nKKhGdPvXaMqN7Ukco0xdDEjgKTs1Ygzg'
+    }
+  ]
+}
+
+
+// This is how we structure our data set in React
+// {
+//   users: [
+//     {
+//       first: 'Bucky',
+//       last: 'Roberts',
+//       image: 'http...',
+//       description: 'I like bacon!'
+//     },
+//     {}
+//   ]
+// }
