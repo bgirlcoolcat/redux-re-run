71766f6fc48a2aff05691e514d1f1c15cf6ef55a
bgirlcoolcat <evansb21@gmail.com>
2018-01-19 13:56:54 +0000

    Add a Provider. Your Provider makes your Store available to whatever is inside the <Provider></Provider> tags.

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
 import App from './components/App';
@@ -8,7 +9,9 @@ import App from './components/App';
 const store = createStore(allReducers);
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 );
 
